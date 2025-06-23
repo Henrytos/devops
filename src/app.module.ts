cf5e58a37ab2adaa17913c22a119242d6c4577ec
Henry <henry.arcaya@sptech.school>
2025-06-22 21:43:21 -0300

    conectando com banco de dados de outro containert

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3333,
+      username: 'admin',
+      password: 'root',
+      database: 'rocketseat-db',
+      entities: [],
+      synchronize: true, // Note: Set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
